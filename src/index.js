55ac2185454733d12f37a96e38d7fac899dc308b
Carolina Pereira <carolina.neverland@gmail.com>
2023-03-21 16:29:46 +0000

    Clean up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,4 +10,3 @@ root.render(
     <App />
   </Router>
 );
-registerServiceWorker();
